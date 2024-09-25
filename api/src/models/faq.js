26b91630627182a9abd2e3d21a5466362596964e
NETMALLORCA <netmallorca@hotmail.com>
2024-09-25 13:53:54 +0200

    20240925 FINAL part1

---
+++ api/src/models/faq.js
@@ -0,0 +1,48 @@
+module.exports = function (sequelize, DataTypes) {
+  const Faq = sequelize.define('Faq',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: DataTypes.DATE
+      }
+    }, {
+      sequelize,
+      tableName: 'faqs',
+      timestamps: true,
+      paranoid: true,
+      indexes: [
+        {
+          name: 'PRIMARY',
+          unique: true,
+          using: 'BTREE',
+          fields: [
+            { name: 'id' }
+          ]
+        }
+      ]
+    }
+  )
+
+  Faq.associate = function (models) {
+
+  }
+
+  return Faq
+}
